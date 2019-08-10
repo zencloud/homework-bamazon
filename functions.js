3c71dd3678f6ca5363114f14f479344d2a3325ed
zencloud <mryanmill@gmail.com>
2019-08-10 17:15:32 -0600

    Dev

--- functions.js
+++ functions.js
@@ -13,7 +13,7 @@ let conn = mysql.createConnection({
 // Connect to Database
 conn.connect(function (err) {
     if (err) throw err;
-    module.exports.items_render();
+    module.exports.render_store_inventory();
 });
 
 
@@ -21,25 +21,77 @@ conn.connect(function (err) {
 module.exports = {
 
     // Welcome Function
-    welcome: function () {
+    render_item_choice: function () {
 
         inq.prompt([
             {
-                name: "question",
-                message: "Hello!"
+                name: "itemID",
+                message: "Please enter the ID of the product you wish to buy:"
+            },
+            {
+                name: "itemAmount",
+                message: "How many would you like to buy?"
+
             }
         ]).then(function (res) {
-            //
+
+            // Set Item ID and Purchase Amount
+            let itemID = res.itemID;
+            let itemAmount = res.itemAmount;
+
+            conn.query("SELECT * FROM store_inventory WHERE ID = ?", [itemID], function (err, res) {
+
+                // Database Error
+                if (err) throw err;
+
+                // Result Invalid
+                if (res.length === 0) {
+                    console.log('Invalid Item ID, please try again');
+                    module.exports.render_item_choice();
+                }
+
+                // Found Result
+                if (res.length > 0) {
+
+                    if (res[0].item_stock - itemAmount < 0) {
+                        console.log('--------------------------------------------------');
+                        console.log('Sorry! Not enough in stock!');
+                        console.log('--------------------------------------------------');
+                        module.exports.render_item_choice();
+                    }
+
+                    if (res[0].item_stock - itemAmount >= 0) {
+
+                        // Purchase config
+                        let cost = res[0].item_price * itemAmount;
+                        let newStock = res[0].item_stock - itemAmount;
+
+                        // Updates to SQL
+                        let updateArray = [
+                            { item_stock: newStock },
+                            { ID: itemID }
+                        ];
+
+                        conn.query("UPDATE store_inventory SET ? WHERE ?", updateArray, function (err, data) {
+
+                            // Print to Screen
+                            console.log('--------------------------------------------------');
+                            console.log(`Congrats! You bought (${itemAmount}) of ${res[0].item_name} for a total of $${cost}.`);
+                            console.log('--------------------------------------------------');
+                            module.exports.render_store_inventory();
+                        });
+                    }
+                }
+            });
+
         });
-
-
     },
 
     //
-    items_render: function () {
+    render_store_inventory: function () {
 
         conn.query("SELECT * FROM store_inventory", function (err, results) {
-                
+
             // Create new table
             let itemTable = new ConsoleTable({
                 head: ['ID', 'Name', 'Cost', 'Stock'],
@@ -53,7 +105,9 @@ module.exports = {
             console.log('Welcome to Bamazon!');
             console.log('Please review our selection:');
             console.log(itemTable.toString());
-            //console.log(results);
+
+            module.exports.render_item_choice();
+
         });
     }
 }
